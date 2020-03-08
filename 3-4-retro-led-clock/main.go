2e89b5e93fff0ca5d31ffbe9a4608d85e2227a85
Mr Gene <yzhydovich@gmail.com>
2020-03-07 19:55:29 -0600

    new

--- 3-4-retro-led-clock/main.go
+++ 3-4-retro-led-clock/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/inancgumus/screen"
 	"fmt"
 	"time"
 )
@@ -115,24 +116,14 @@ var (
 		nine,
 	}
 
-	clock = [8][5]string{}
 
 	h1, h2, m1, m2, s1, s2 int
 )
 
 func main() {
-	showDots := true
 
-	for y := range digits[0] {
-		for i := range digits {
-			fmt.Printf("%v ", digits[i][y])
-		}
-		fmt.Println()
-	}
-	
-	fmt.Println()
-
-	// for {
+	for {
+		screen.MoveTopLeft()
 		h, m, s := time.Now().Clock()
 
 		if h >= 10 {
@@ -159,27 +150,32 @@ func main() {
 			s2 = s % 10
 		}
 
-		clock = [8][5]string{digits[h1],digits[h2],digits[m1],digits[m2],digits[s1],digits[s2]}
-
-		if showDots {
-			fmt.Printf("%s %s %s %s %s %s %s %s\n", clock[0][0], clock[1][0], dots[0], clock[2][0], clock[3][0], dots[0], clock[4][0], clock[5][0])
-			fmt.Printf("%s %s %s %s %s %s %s %s\n", clock[0][1], clock[1][1], dots[1], clock[2][1], clock[3][1], dots[1], clock[4][1], clock[5][1])
-			fmt.Printf("%s %s %s %s %s %s %s %s\n", clock[0][2], clock[1][2], dots[2], clock[2][2], clock[3][2], dots[2], clock[4][2], clock[5][2])
-			fmt.Printf("%s %s %s %s %s %s %s %s\n", clock[0][3], clock[1][3], dots[3], clock[2][3], clock[3][3], dots[3], clock[4][3], clock[5][3])
-			fmt.Printf("%s %s %s %s %s %s %s %s\n", clock[0][4], clock[1][4], dots[4], clock[2][4], clock[3][4], dots[4], clock[4][4], clock[5][4])
-			showDots = false
-		} else {
-			fmt.Printf("%s %s %s %s %s %s %s %s\n", clock[0][0], clock[1][0], nodots[0], clock[2][0], clock[3][0], nodots[0], clock[4][0], clock[5][0])
-			fmt.Printf("%s %s %s %s %s %s %s %s\n", clock[0][1], clock[1][1], nodots[1], clock[2][1], clock[3][1], nodots[1], clock[4][1], clock[5][1])
-			fmt.Printf("%s %s %s %s %s %s %s %s\n", clock[0][2], clock[1][2], nodots[2], clock[2][2], clock[3][2], nodots[2], clock[4][2], clock[5][2])
-			fmt.Printf("%s %s %s %s %s %s %s %s\n", clock[0][3], clock[1][3], nodots[3], clock[2][3], clock[3][3], nodots[3], clock[4][3], clock[5][3])
-			fmt.Printf("%s %s %s %s %s %s %s %s\n", clock[0][4], clock[1][4], nodots[4], clock[2][4], clock[3][4], nodots[4], clock[4][4], clock[5][4])
-			showDots = true
+		clock := [8][5]string{
+			digits[h1],
+			digits[h2],
+			dots,
+			digits[m1],
+			digits[m2],
+			dots,
+			digits[s1],
+			digits[s2],
 		}
 
-		
-		time.Sleep(1 * time.Second)
-	// }
-	
+		if s2 == 0 || s2%2 == 0 {
+			clock[2][1], clock[2][3], clock[5][1], clock[5][3] = "░", "░", "░", "░"
+		}
 
+		for y := range clock[0] {
+			for i := range clock {
+				fmt.Printf("%v ", clock[i][y])
+			}
+			fmt.Println()
+		}
+
+		time.Sleep(1 * time.Second)
+
+		screen.Clear()
+	}
+
+	
 }
